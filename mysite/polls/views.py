ea3c23f35f873112bbbc56839fa689249eb8b8a5
vibeznstuff <richard.dansoh@gmail.com>
2019-11-27 12:43:44 -0500

    Added zombie game to test using javascript in templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -53,3 +53,7 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def zombies(request):
+    return render(request,'polls/zombies.html')
